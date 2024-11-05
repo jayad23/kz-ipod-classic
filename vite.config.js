c22b6698fe7bdcbf7088ebc5f17daac0243a1a24
Kz <iamkikevanegas@gmail.com>
2024-11-06 00:36:01 +0100

    feat: styles context

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,50 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
+const manifestForPlugin = {
+  registerType: "prompt",
+  includeAssets: ["favicon.ico", "apple-touch-icon.png", "maskable_icon.png"],
+  manifest: {
+    short_name: "Kz iPod",
+    name: "Kz iPod Classic",
+    description: "Reproductor personalizado con máscara de iPod Classic",
+    icons: [
+      {
+        src: "./maskable_icon.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "Maskable",
+      },
+      {
+        src: "./logo192.png",
+        type: "image/png",
+        sizes: "192x192",
+      },
+      {
+        src: "./logo256.png",
+        type: "image/png",
+        sizes: "256x256",
+      },
+      {
+        src: "./logo384.png",
+        type: "image/png",
+        sizes: "384x384",
+      },
+      {
+        src: "./logo512.png",
+        type: "image/png",
+        sizes: "512x512",
+      },
+    ],
+    start_url: "/",
+    scope: "/",
+    display: "standalone",
+    theme_color: "#000000",
+    background_color: "#ffffff",
+  },
+};
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), VitePWA(manifestForPlugin)],
+});
